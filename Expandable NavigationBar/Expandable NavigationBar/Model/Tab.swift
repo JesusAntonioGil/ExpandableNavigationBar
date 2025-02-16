56d42af47234ee3142b6a6b60d9deb8a006ee6ee
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-16 13:04:30 +0100

    Added expandable navigation bar

---
+++ Expandable NavigationBar/Expandable NavigationBar/Model/Tab.swift
@@ -0,0 +1,16 @@
+//
+//  Tab.swift
+//  Expandable NavigationBar
+//
+//  Created by Jesus Antonio Gil on 16/2/25.
+//
+
+import Foundation
+
+
+enum Tab:String, CaseIterable {
+    case all = "All"
+    case personal = "Personal"
+    case office = "Updates"
+    case community = "Gaming"
+}
--- Expandable NavigationBar/Expandable NavigationBar/View/Home.swift
+++ Expandable NavigationBar/Expandable NavigationBar/View/Home.swift
@@ -10,6 +10,10 @@ import SwiftUI
 
 struct Home: View {
     @State private var searchText: String = ""
+    @State private var activeTab: Tab = .all
+    @Environment(\.colorScheme) private var scheme
+    @Namespace private var animation
+    
     
     var body: some View {
         ScrollView(.vertical) {
@@ -27,28 +31,67 @@ struct Home: View {
     
     @ViewBuilder
     private func ExpandableNaigationBar(_ title: String = "Messages") -> some View {
-        VStack(spacing: 10) {
-            Text(title)
-                .font(.largeTitle.bold())
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.bottom, 10)
+        GeometryReader { proxy in
+            let minY = proxy.frame(in: .scrollView(axis: .vertical)).minY
             
-            HStack(spacing: 12) {
-                Image(systemName: "magnifyingglass")
-                    .font(.title3)
+            VStack(spacing: 10) {
+                // Title
+                Text(title)
+                    .font(.largeTitle.bold())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 10)
                 
-                TextField("Search Conversations", text: $searchText)
+                // Search Bar
+                HStack(spacing: 12) {
+                    Image(systemName: "magnifyingglass")
+                        .font(.title3)
+                    
+                    TextField("Search Conversations", text: $searchText)
+                }
+                .padding(.vertical, 10)
+                .padding(.horizontal, 15)
+                .frame(height: 45)
+                .background {
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(.background)
+                }
+                
+                //Custom Segemented Picker
+                ScrollView(.horizontal) {
+                    HStack(spacing: 12) {
+                        ForEach(Tab.allCases, id:\.rawValue) { tab in
+                            Button {
+                                withAnimation(.snappy) {
+                                    activeTab = tab
+                                }
+                            } label: {
+                                Text(tab.rawValue)
+                                    .font(.callout)
+                                    .foregroundStyle(activeTab == tab ? (scheme == .dark ? .black : .white) : Color.primary)
+                                    .padding(.vertical, 8)
+                                    .padding(.horizontal, 15)
+                                    .background {
+                                        if activeTab == tab {
+                                            Capsule()
+                                                .fill(Color.primary)
+                                                .matchedGeometryEffect(id: "ACTIVEBAR", in: animation)
+                                        } else {
+                                            Capsule()
+                                                .fill(.background)
+                                        }
+                                    }
+                            }
+                            .buttonStyle(.plain)
+                        }
+                    }
+                }
+                .frame(height: 50)
             }
-            .padding(.vertical, 10)
-            .padding(.horizontal, 15)
-            .frame(height: 45)
-            .background {
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(.background)
-            }
+            .padding(.top, 25)
+            .safeAreaPadding(.horizontal, 15)
+            .offset(y: minY < 0 ? -minY : 0)
         }
-        .padding(.top, 25)
-        .padding(.horizontal, 15)
+        .frame(height: 190)
         .padding(.bottom, 10)
     }
     
