32ba5d4535b2516f644227983236b60c0dcb8a8e
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-16 11:55:17 +0100

    Create Project

---
+++ Expandable NavigationBar/Expandable NavigationBar/Expandable_NavigationBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expandable_NavigationBarApp.swift
+//  Expandable NavigationBar
+//
+//  Created by Jesus Antonio Gil on 16/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Expandable_NavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
